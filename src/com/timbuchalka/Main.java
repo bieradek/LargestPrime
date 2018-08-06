db01677c53bd04d2de0ed71f7856e602eaadbd69
Radoslaw Biedrzycki <radoslaw.biedrzycki@syncron.com>
2018-08-06 23:08:51 +0200

    Finally working solution

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -8,14 +8,9 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getLargestPrime(45));
         System.out.println("*************************");
-        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(100));
         System.out.println("*************************");
-        System.out.println(getLargestPrime(3242));
-        System.out.println("*************************");
-        System.out.println(getLargestPrime(324224128));
-
     }
 
     public static int getLargestPrime(int number) {
@@ -23,37 +18,19 @@ public class Main {
             return -1;
         }
         int copy = number;
-        int divisorCount = 0;
-        int nextDivisor = 1;
         for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                System.out.println("Divisor found and equal to " + i);
-                divisorCount++;
+
+
+            if (copy % i == 0) {
+                copy /= i; // 100/2-->50
+                if (copy == 1) {
+                    copy = i;
+                    break;
+                }
+                //System.out.println("Division by " + i + " is " + copy);
+                i--;
             }
         }
-        return divisorCount;
+        return copy;
     }
 }
-
-/*
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-15 3 * 5
-14 2 * 7
-18 2 * 3 * 3
-22 2 * 11
-24 2 * 2 * 2  * 3
-25 5 * 5
-217 7
-*/
